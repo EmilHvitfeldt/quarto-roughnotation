0773adbdbb67eca91c2e2c8eb53a842200c49a07
Garrick Aden-Buie <garrick@adenbuie.com>
2022-09-12 11:21:42 -0400

    Move initialization script into `rough.js`

--- _extensions/roughnotation/rough.lua
+++ _extensions/roughnotation/rough.lua
@@ -2,7 +2,6 @@ function Meta(m)
   quarto.doc.addHtmlDependency({
     name = "roughnotation",
     version = "1.0.0",
-    scripts = {"assets/rough-notation.iife.js"}
+    scripts = {"assets/rough-notation.iife.js", "rough.js"}
   })
-  quarto.doc.includeFile("after-body", "assets/roughnotation.html")
 end
